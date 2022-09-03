b55d7da26a2215cc7b62133cc4a9abc542dfc9b8
Rabiullah-Rabi <mdrabiullah789@gmail.com>
2022-09-03 15:56:59 +0600

    check data available or not

--- fullNews.js
+++ fullNews.js
@@ -6,32 +6,32 @@ const loadDetails = async (newsID) => {
     displayDetails(details.data[0]);
 }
 const displayDetails = (news) => {
+    const { image_url, title, details, author, total_view } = news;
     const modalbody = document.getElementById('modal-body');
     modalbody.innerHTML = '';
     const newsContainer = document.createElement('div');
     newsContainer.innerHTML = `
-        <div class="card mb-5 border-0 shadow rounded p-3">
+        <div class="card mb-5 border-0 p-3">
             <div class="row g-0">
                 <div class="col-12">
-                    <img src="${news.image_url}" class="img-fluid rounded h-100" alt="...">
+                    <img src="${isAvailable(image_url)}" class="img-fluid rounded h-100" alt="...">
                 </div>
                 <div class="col-12">
                     <div class="card-body border-0">
-                        <h3 class="card-title">${news.title}</h3>
-                        <h6>Category:</h6>
-                        <p class="card-text">${news.details}</p>
+                        <h3 class="card-title">${isAvailable(title)}</h3>
+                        <p class="card-text">${isAvailable(details)}</p>
                         <div class="row">
                             <div class="col-8 d-flex align-items-center">
-                                <img src="${news.author.img}" alt="" class="img-fluid rounded-circle"
+                                <img src="${isAvailable(author.img)}" alt="" class="img-fluid rounded-circle"
                                     height="50" width="50">
                                 <div class="ms-2">
-                                    <h6>${news.author.name}</h6>
-                                    <p class="mb-0">${news.author.published_date}</p>
+                                    <h6>${isAvailable(author.name)}</h6>
+                                    <p class="mb-0">${isAvailable(author.published_date)}</p>
                                 </div>
                             </div>
                             <div class="col-4 d-flex align-items-center justify-content-center">
                                 <img src="images/carbon_view.png" alt="" height="20">
-                                <p class="ms-3 mb-0 fw-bold">${news.total_view}</p>
+                                <p class="ms-3 mb-0 fw-bold">${isAvailable(total_view)}</p>
                             </div>
                         </div>
                     </div>
--- news.js
+++ news.js
@@ -6,6 +6,15 @@ const loadNews = async (catID) => {
     const newses = await res.json();
     displaynewses(newses.data);
 }
+//Check data available or not
+const isAvailable = (value) => {
+    if (value !== null) {
+        return value;
+    }
+    else {
+        return "No data available";
+    }
+}
 //display news news
 const displaynewses = (newses) => {
     const newsContainer = document.getElementById('all-news');
@@ -13,6 +22,7 @@ const displaynewses = (newses) => {
     //Count news with category
     const message = document.getElementById('message');
     const totalNews = newses.length;
+    //message for no news found
     if (totalNews > 0) {
         message.innerText = `${totalNews} items found`;
     }
@@ -22,29 +32,32 @@ const displaynewses = (newses) => {
         notFound.innerHTML = `<img src="images/product-not-found.jpg" class="img-fluid rounded h-100" alt="...">`
         newsContainer.appendChild(notFound)
     }
+    //news card
     newses.forEach(news => {
+        const { _id, image_url, title, details, author, total_view } = news;
+        console.log(news);
         const newsitem = document.createElement('div');
         newsitem.innerHTML = `
-        <div class="card mb-5 border-0 shadow rounded p-3" onclick="loadDetails('${news._id}')" data-bs-toggle="modal" data-bs-target="#fullNews">
+        <div class="card mb-5 border-0 shadow rounded p-3" onclick="loadDetails('${_id}')" data-bs-toggle="modal" data-bs-target="#fullNews">
             <div class="row g-0">
                 <div class="col-md-4">
-                    <img src="${news.image_url}" class="img-fluid rounded h-100" alt="...">
+                    <img src="${isAvailable(image_url)}" class="img-fluid rounded h-100" alt="...">
                 </div>
                 <div class="col-md-8">
                     <div class="card-body border-0">
-                        <h3 class="card-title">${news.title}</h3>
-                        <p class="card-text">${news.details.slice(0 - 200)}...</p>
+                        <h3 class="card-title">${isAvailable(title)}</h3>
+                        <p class="card-text">${isAvailable(details).slice(0 - 200)}...</p>
                         <div class="row">
                             <div class="col-4 d-flex align-items-center">
-                                <img src="${news.author.img}" alt="" class="img-fluid rounded-circle" height="50" width="50">
+                                <img src="${isAvailable(author.img)}" alt="" class="img-fluid rounded-circle" height="50" width="50">
                                 <div class="ms-2">
-                                    <h6>${news.author.name}</h6>
-                                    <p class="mb-0">${news.author.published_date}</p>
+                                    <h6>${isAvailable((author.name))}</h6>
+                                    <p class="mb-0">${isAvailable(author.published_date)}</p>
                                 </div>
                             </div>
                             <div class="col-2 d-flex align-items-center justify-content-center">
                                 <img src="images/carbon_view.png" alt="" height="20">
-                                <p class="ms-3 mb-0 fw-bold">${news.total_view}</p>
+                                <p class="ms-3 mb-0 fw-bold">${isAvailable(total_view)}</p>
                             </div>
                             <div class="col-3 d-flex align-items-center justify-content-center">
                                 <img src="images/Group 116134.png" alt="" class="img-fluid">
@@ -68,6 +81,9 @@ const isLoading = (loading) => {
     const spinnerContainer = document.getElementById('spinner');
     if (loading) {
         spinnerContainer.classList.remove('d-none');
+        const newsContainer = document.getElementById('all-news');
+        newsContainer.innerHTML = '';
+
     }
     else {
         spinnerContainer.classList.add('d-none');
